da921908012ddf22c34e4feef28222859f836e61
ManuelMaciel <manuelmaciel7001@gmail.com>
2021-09-20 21:28:29 -0400

    feat: added new package: card swipper innto card swipper widget and added images and placeholder

--- lib/src/widgets/card_swiper.dart
+++ lib/src/widgets/card_swiper.dart
@@ -1,14 +1,36 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 
 class CardSwiper extends StatelessWidget {
-  const CardSwiper({ Key? key }) : super(key: key);
+  const CardSwiper({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Container(
       width: double.infinity,
-      height: 500,
-      color: Colors.deepPurple.shade400,
+      height: size.height * 0.5,
+      // color: Colors.deepPurple.shade400,
+      child: Swiper(
+        itemCount: 10,
+        layout: SwiperLayout.STACK,
+        itemHeight: size.height * 0.4,
+        itemWidth: size.width * 0.6,
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: () => Navigator.pushNamed(context, 'details', arguments: 'movie-instance'),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: FadeInImage(
+                  placeholder: NetworkImage('https://via.placeholder.com/300x400'),
+                  image: NetworkImage('https://via.placeholder.com/300x400'),
+                  fit: BoxFit.cover,
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
